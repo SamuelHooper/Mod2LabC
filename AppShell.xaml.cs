1239f530d7beeb976b7d8d7cfd622ab34db276da
Samuel Hooper <shooper3@my.wctc.edu>
2024-04-11 18:00:02 -0500

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Mod2LabC
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace Mod2LabC
+{
+    public partial class MainPage : ContentPage
+    {
+        string LetterOne = "";
+        string LetterTwo = "";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AddLetterOne(object sender, EventArgs e)
+        {
+            string nextLetter = ((Button)sender).Text;
+            if (nextLetter == "Space")
+            {
+                MessageOne.Text += Morse.MorseCoder(LetterOne);
+                LetterOne = "";
+            }
+            else
+            {
+                LetterOne += nextLetter;
+            }
+        }
+
+        private void AddLetterTwo(object sender, EventArgs e)
+        {
+            string nextLetter = ((Button)sender).Text;
+            if (nextLetter == "Space")
+            {
+                MessageTwo.Text += Morse.MorseCoder(LetterTwo);
+                LetterTwo = "";
+            }
+            else
+            {
+                LetterTwo += nextLetter;
+            }
+        }
+    }
+
+}
